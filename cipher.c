1568b6964dee0a0f9e63a322fd21782c284fef01
Kevin Hock <kevin.hock@stonybrook.edu>
2013-09-11 23:40:12 -0400

    did adam styling

--- cipher.c
+++ cipher.c
@@ -1,7 +1,8 @@
+// do file stuff without blowfish
+// check at least 2 args first
 // error check syscalls and malloc
 // ?how to test same file?
 // autoupdate variables $Revision$ or $ld$
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,19 +12,15 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include "blowfish.h"
+#include "cipher.h"
 
-void err_quit(char *errmsg){
-  fputs(errmsg, stderr);
-  fflush(NULL);
-  exit(1);
-}
+int ipf=0;
 
-int main(int argc, char **argv)
-{
-  printf("Hello World\n");
+int main(int argc, char **argv){
   int ch, dflag=0, eflag=0, pflag=0; 
   char* password;
 
+  jessie();
   while ((ch = getopt(argc, argv, "p:devh")) != -1) {
      switch (ch) {
      case 'd':
@@ -45,9 +42,8 @@ int main(int argc, char **argv)
               exit(1);
               break;
      case 'p':
-              password = malloc(strlen(optarg+1));
-              if(!password)
-                err_quit("malloc failed");
+              if(!(password = malloc(strlen(optarg+1))))
+                malloc_fail(__LINE__);  
               strcpy(password, optarg);
               printf("password is %s\n", password);
               pflag=1;
@@ -64,7 +60,8 @@ int main(int argc, char **argv)
   }
   
   if (pflag==0){
-    password = malloc(_PASSWORD_LEN+1);
+    if(!(password = malloc(_PASSWORD_LEN+1)))
+      malloc_fail(__LINE__);
     strcpy(password,getpass("Enter Password:"));
     printf("Password is %s\n", password);   
   }
@@ -93,7 +90,7 @@ int main(int argc, char **argv)
     // Make sure it's a file and not anything special
     // What could go wrong with open and read
     printf("ALL GOOD I GUESS\n");
-    open(*(argv+argc-2),O_RDONLY,0);
+    ipf = open(*(argv+argc-2),O_RDONLY,0);
   }
 
   if (!strcmp(*(argv+argc-1),"-")){
@@ -115,12 +112,34 @@ int main(int argc, char **argv)
 
   int pgsize = getpagesize();
   // calloc the last page and make it the mode of page size into file size
-
-
   char from[128]; 
+  char *hehe;
+  hehe = malloc(128);
+  int amt_read = read(ipf,from,128);
+  if(amt_read != 128){
+    // Partial Read
+  }
+  printf("from is %s.\nfrom length is %lu", from, strlen(from));
+
+  // while(n=read(STDIN_FILENO, buf, BUFFSIZE) == 128){
+    // if(write(STDOUT_FILENO, buf, n) != n)
+      // fuck;
+  // }
+
+  // if(n>0)
+    // readfails;
+  
+
   char to[128];
   int len = 128;
-  strcpy(from, "AAAAAAAAA");
+  int i;
+  
+  strcpy(from, "A");
+  for(i=0; i<126;i++)
+    strcat(from, "A");
+
+
+
   /* define a structure to hold the key */
   BF_KEY key;
 
@@ -146,9 +165,95 @@ int main(int argc, char **argv)
   printf("from is %s\n", from);
   printf("TO IS %s\n", to);
   /* Decrypting is the same: just pass BF_DECRYPT instead */
-  BF_cfb64_encrypt(to, from, 128, &key, iv, &n, BF_DECRYPT);
+  BF_cfb64_encrypt(from, to, 128, &key, iv, &n, BF_DECRYPT);
 
   printf("from is %s\n", from);
-  printf("TO IS %s\n", to);
+  printf("TO IS %s\nto.length is %lu", to, strlen(to));
 
+
+  /*
+   * This is how you encrypt an input char* buffer "from", of length "len"
+   * onto output buffer "to", using key "key".  Jyst pass "iv" and "&n" as
+   * shown, and don't forget to actually tell the function to BF_ENCRYPT.
+   */
+  // BF_cfb64_encrypt(from, to, len, &key, iv, &n, BF_ENCRYPT);
+
+  /* Decrypting is the same: just pass BF_DECRYPT instead */
+  // BF_cfb64_encrypt(from, to, len, &key, iv, &n, BF_DECRYPT);
+
+   printf("lol %lu\n", strlen("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
+  return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void err_quit(char *errmsg, int linenumber){
+  fputs(errmsg, stderr);
+  fflush(NULL);
+  exit(1);
+}
+
+void jessie(){
+  printf("          _______  _______  _______  _______  _______  _______          \n");
+  printf("|\\     /|(  ____ \\(  ____ \\(  ____ \\(  ___  )(  ___  )(  ___  )|\\     /|\n");
+  printf("( \\   / )| (    \\/| (    \\/| (    \\/| (   ) || (   ) || (   ) || )   ( |\n");
+  printf(" \\ (_) / | (__    | (__    | (__    | (___) || (___) || (___) || (___) |\n");
+  printf("  \\   /  |  __)   |  __)   |  __)   |  ___  ||  ___  ||  ___  ||  ___  |\n");
+  printf("   ) (   | (      | (      | (      | (   ) || (   ) || (   ) || (   ) |\n");
+  printf("   | |   | (____/\\| (____/\\| (____/\\| )   ( || )   ( || )   ( || )   ( |\n");
+  printf("   \\_/   (_______/(_______/(_______/|/     \\||/     \\||/     \\||/     \\|\n");
+  printf("                                                                        \n");
+  printf(" ______   _        _______          \n");
+  printf("(  ___ \\ ( \\      (  ___  )|\\     /|\n");
+  printf("| (   ) )| (      | (   ) || )   ( |\n");
+  printf("| (__/ / | |      | |   | || | _ | |\n");
+  printf("|  __ (  | |      | |   | || |( )| |\n");
+  printf("| (  \\ \\ | |      | |   | || || || |\n");
+  printf("| )___) )| (____/\\| (___) || () () |\n");
+  printf("|/ \\___/ (_______/(_______)(_______)\n");
+  printf("                                    \n");
+  printf(" _______ _________ _______          _________ _       \n");
+  printf("(  ____ \\\\__   __/(  ____ \\|\\     /|\\__   __/( (    /|\n");
+  printf("| (    \\/   ) (   | (    \\/| )   ( |   ) (   |  \\  ( |\n");
+  printf("| (__       | |   | (_____ | (___) |   | |   |   \\ | |\n");
+  printf("|  __)      | |   (_____  )|  ___  |   | |   | (\\ \\) |\n");
+  printf("| (         | |         ) || (   ) |   | |   | | \\   |\n");
+  printf("| )      ___) (___/\\____) || )   ( |___) (___| )  \\  |\n");
+  printf("|/       \\_______/\\_______)|/     \\|\\_______/|/    )_)\n");
+  printf("                                                      \n");
+  printf("_________         _________ _______             _______  _ \n");
+  printf("\\__   __/|\\     /|\\__   __/(  ____ \\  |\\     /|(  ____ )( )\n");
+  printf("   ) (   | )   ( |   ) (   | (    \\/  | )   ( || (    )|| |\n");
+  printf("   | |   | (___) |   | |   | (_____   | |   | || (____)|| |\n");
+  printf("   | |   |  ___  |   | |   (_____  )  | |   | ||  _____)| |\n");
+  printf("   | |   | (   ) |   | |         ) |  | |   | || (      (_)\n");
+  printf("   | |   | )   ( |___) (___/\\____) |  | (___) || )       _ \n");
+  printf("   )_(   |/     \\|\\_______/\\_______)  (_______)|/       (_)\n");
+  printf("                                                           \n");
+}
+
+void malloc_fail(int linenumber){
+  err_quit("malloc fail @ line %d \n",linenumber);
+}
+
+
